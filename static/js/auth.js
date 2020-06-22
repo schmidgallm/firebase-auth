34450d06bf9e230f2feab1591075b274974dda60
schmidgallm <schmidgallm@outlook.com>
2020-06-21 22:02:34 -0700

    add firebase auth signin, logout, signup, and auth status change methods

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,7 +1,15 @@
+// Auth status changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log('User logged in', user);
+  } else {
+    console.log('User not logged in');
+  }
+});
+
 // Sign up
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
-  // prevent form from submitting
   e.preventDefault();
 
   // init user values
@@ -11,10 +19,57 @@ signupForm.addEventListener('submit', (e) => {
   };
 
   //   Sing up user to firebase
-  auth.createUserWithEmailAndPassword(user.email, user.password).then((res) => {
-    // close modal after successful response and clear form
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
+  auth
+    .createUserWithEmailAndPassword(user.email, user.password)
+    .then((res) => {
+      // TODO
+      // send verification email
+
+      // close modal after successful response and clear form
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+});
+
+// Logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
   });
+});
+
+// Signin
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // init user values
+  const user = {
+    email: loginForm['login-email'].value,
+    password: loginForm['login-password'].value,
+  };
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((res) => {
+      // close modal after successful response and clear form
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
 });
