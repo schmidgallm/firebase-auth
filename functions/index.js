1896a2f3e3219be974e48eb78a91d6519a91f819
schmidgallm <schmidgallm@outlook.com>
2020-06-24 12:13:52 -0700

    add conditional to check if user is admin before making other admins

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,12 @@ admin.initializeApp();
 
 // add admin role function
 exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check request is made by admin
+  if (context.auth.token.admin !== true) {
+    return {
+      error: 'Only admins can add other admins',
+    };
+  }
   // get user and add custom claim (admin)
   return admin
     .auth()
